1f4cd2292a814c00ec1bd05e4f9151fe1f39755a
omonimus <davidepollicino2015@gmail.com>
2020-10-26 12:44:53 +0000

    Create AI coursework fle

---
+++ coursework/40401270.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string get_command_line_parameter()
+{
+    int number_arguments; 
+}
+
+int main(int argc, char** argv) 
+{
+
+    if(argc < 2)
+        cout<< "Sono 2"<<endl;
+    else
+        cout << "Tree";
+
+    return 0; 
+}
---
+++ coursework/caveroute.bat/caveroute.bat
@@ -0,0 +1,40 @@
+
+@echo off
+set STARTTIME=%TIME%
+
+rem - insert here the line that runs your program - make sure you pass through the file name appropriately
+rem examples are:
+rem myprogram.exe %1
+rem or
+rem java -jar myprogram.jar %1
+
+
+
+set ENDTIME=%TIME%
+
+  
+
+
+    rem Change formatting for the start and end times
+    for /F "tokens=1-4 delims=:.," %%a in ("%STARTTIME%") do (
+       set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+    )
+
+    for /F "tokens=1-4 delims=:.," %%a in ("%ENDTIME%") do (
+       set /A "end=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+    )
+
+    rem Calculate the elapsed time by subtracting values
+    set /A elapsed=end-start
+
+    rem Format the results for output
+    set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+    if %hh% lss 10 set hh=0%hh%
+    if %mm% lss 10 set mm=0%mm%
+    if %ss% lss 10 set ss=0%ss%
+    if %cc% lss 10 set cc=0%cc%
+
+    set DURATION=%hh%:%mm%:%ss%.%cc%
+
+    
+    echo Duration : %DURATION% 
