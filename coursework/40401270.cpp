589d61ab35083fb35c4e1abbd670671ad2d6e4d9
Davide Pollicino <davidepollicino2015@gmail.com>
2020-10-26 14:53:39 +0000

    Reads input from file

--- coursework/40401270.cpp
+++ coursework/40401270.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sstream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 bool DoesFileExists(string fileName)
@@ -25,7 +27,8 @@ int main(int argc, char** argv)
     else
     {
         string input_file_name = argv[1];
-        input_file_name += ".bat";
+        string output_file_name = input_file_name + ".csn";
+        input_file_name += ".cav";
         if(! DoesFileExists(input_file_name))
         {
             cout <<"The file name indicated has not been found, mind to indicate the file without the format extensioon .bat"<<endl;
@@ -34,17 +37,37 @@ int main(int argc, char** argv)
         else
         {
             cout <<"File found"<<endl;
+
+            // Open stream file, il primo numero e' il numero di vertici nel grafo; 
+            ifstream infile; 
+             infile.open(input_file_name);
+    
+                // Read Input
+                vector<int> vect;
+                std::string str;
+                while (getline(infile ,str)) {
+                    std::stringstream ss(str);
+
+                    for (int i; ss >> i;) {
+                        vect.push_back(i);    
+                        if (ss.peek() == ',')
+                            ss.ignore();
+                    }
+                    
+                    for (int i = 0; i < vect.size(); i++)
+                        cout << vect[i] << " ";
+                }
+
+                // Store number of vertext; 
+                unsigned long int number_caves = vect[0];
+
+                /*
+                    // Read boolean for adjency matrix
+                    for(int i = number_caves*2 +1; i< vect.size(); i++);
+                */
+
         }
     }
-    
-    /*
-    // Loop through each argument and print its number and value
-    for (int count{ 0 }; count < argc; ++count)
-    {
-        cout << count << ' ' << argv[count] << '\n';
-    }
-    */
-
 
     return 0; 
 }
